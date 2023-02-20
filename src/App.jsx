bb7e0c9fca2947d20b5e9193e1d12734df13ef0c
himarnoel <olaimarnoel@gmail.com>
2023-02-21 00:37:50 +0100

    ecomerce

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+axios
+  return (
+    <div className="">
+    
+   
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Landpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landpage = () => {
+  return (
+    <div>Landpage</div>
+  )
+}
+
+export default Landpage
+
+{/* <CiSearch/>
+<FiUser/>
+<RxHeart/>
+<FiShoppingCart/> */}
