719d0f207c146229f1ab80792c4c3b4a242fc519
himarnoel <olaimarnoel@gmail.com>
2023-02-21 01:26:08 +0100

    changed logo

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../assets/Logo.svg";
 const Nav = () => {
-  return (
-    <div className="absolute top-4 text-white z-40 ">
-        dfadsd
-    </div>
-  )
-}
+  return <div className="absolute top-4 text-white z-40 ">
+    <img src={logo} alt=""  className="w-32"/>
+  </div>;
+};
 
-export default Nav
+export default Nav;
