ff90ab0a59a86474099bc2d20abfb37421688d20
himarnoel <olaimarnoel@gmail.com>
2023-02-21 01:19:57 +0100

    added and remaned logo

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className="absolute top-4">
+    <div className="absolute top-4 text-white z-40 ">
         dfadsd
     </div>
   )
